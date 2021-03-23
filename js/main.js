7495edc4d2cf213f98f0007e6f82e270e2a70e83
Ivanin Alexandr <ivanin.alexandr1994@yandex.ru>
2021-03-23 17:48:42 +0300

    complete homework 1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,43 @@
 const mySwiper = new Swiper('.swiper-container', {
-	loop: true,
+  loop: true,
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.slider-button-next',
-		prevEl: '.slider-button-prev',
-	},
-});
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+})
+//------Scroll Links smooth--------
+const scrollLinks = Array.from(document.querySelectorAll('.scroll-link'))
+const scrollLinksHandler = arrayLinks => {
+  arrayLinks.forEach(item => {
+    item.addEventListener('click', e => {
+      e.preventDefault()
+      const href = item.getAttribute('href')
+      document.querySelector(href).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      })
+    })
+  })
+}
+scrollLinksHandler(scrollLinks)
 
+//-------Cart---------
+const cartBtn = document.querySelector('.button-cart')
+const overlay = document.querySelector('.overlay')
+
+const cartBtnHandler = e => {
+	e.preventDefault()
+	overlay.classList.add('show')
+}
+cartBtn.addEventListener('click', cartBtnHandler)
+
+function modalHandler(e){
+	if(e.target.classList.contains('modal-close')){
+			this.classList.remove('show')
+	}else if(e.target.classList.contains('overlay')){
+			this.classList.remove('show')
+	}
+}
+overlay.addEventListener('click', modalHandler)
